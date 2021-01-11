ba67003ad2ba97aff1648eb7d8b244cc6b3400ac
gk-svg <gk32239@gmail.com>
2021-01-11 18:13:22 +0530

    geted data from API and created model

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+import requests
+from django.http import HttpResponse
 # Create your views here.
+
+def Home(request):
+    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    data = requests.get(url).json()
+    return render(request,'home.html',{'data':data})
